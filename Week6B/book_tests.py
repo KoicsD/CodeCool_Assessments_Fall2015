4b6914ba9c6e68ee2e252363d20238a60add58cb
Daniel Koics <koics.dani@gmail.com>
2015-11-27 13:53:32 +0100

    Week6B tasks

---
+++ Week6B/book_tests.py
@@ -0,0 +1,34 @@
+import unittest
+from unittest import mock
+
+from book import Book
+
+
+class BookTestSuite(unittest.TestCase):
+    def test_init_correctParameters_setAllNecessaryAttributes(self):
+        b = Book("132350882", "Clean Code", "Robert C. Martin")
+        self.assertEqual(b.isbn, "132350882", "-0.25: ISBN is not set!")
+        self.assertEqual(b.title, "Clean Code", "-0.25: title is not set!")
+        self.assertEqual(b.author, "Robert C. Martin", "-0.25: author is not set!")
+        self.assertIsNone(b.price, "-0.25: price is not set!")
+
+    def test_set_price_10_setThePriceTo10(self):
+        b = Book("132350882", "Clean Code", "Robert C. Martin")
+        expected_price = 10
+        b.set_price(expected_price)
+        self.assertEqual(expected_price, b.price)
+
+    def test_open_noArgs_printCorrectText(self):
+        title = "Clean Code"
+        author = "Robert C. Martin"
+        b = Book("132350882", title, author)
+        print_mock = mock.MagicMock()
+
+        with mock.patch("builtins.print", print_mock):
+            b.open()
+
+        print_mock.assert_called_with("The {} written by {} is opened.".format(title, author))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Week6B/max_test.py
@@ -0,0 +1,27 @@
+import unittest
+from assessment.max import max
+
+
+class MaxTestCase(unittest.TestCase):
+
+    def test_Max_FirstIsGreater_returnFirst(self):
+        self.assertEqual(2, max(2, 1))
+
+    def test_Max_SecondIsGreater_returnSecond(self):
+        self.assertEqual(2, max(1, 2))
+
+    def test_Max_Equals_returnAny(self):
+        self.assertEqual(2, max(2, 2))
+
+    def test_Max_FirstIsGreaterAndNegative_returnFirst(self):
+        self.assertEqual(-1, max(-1, -2))
+
+    def test_Max_SecondIsGreaterAndNegative_returnSecond(self):
+        self.assertEqual(-1, max(-2, -1))
+
+    def test_Max_EqualsAndNegative_returnAny(self):
+        self.assertEqual(-2, max(-2, -2))
+
+
+if __name__ == '__main__':
+    unittest.main()
